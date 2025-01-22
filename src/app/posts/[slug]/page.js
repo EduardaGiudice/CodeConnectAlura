5a76d56e63f2b551106c78d80a0e16607c0fc327
Eduarda Giudice <eduardagiudice1@gmail.com>
2025-01-21 22:05:50 -0300

    fix errors

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -2,7 +2,7 @@ import { remark } from "remark";
 import html from "remark-html";
 
 import styles from "./page.module.css";
-import { CardPost } from "@/components/CardPost";
+import { CardPost } from "@/components/CardPost/Index";
 import db from "../../../../prisma/db";
 import { redirect } from "next/navigation";
 
--- src/components/SearchForm/Index.jsx
+++ src/components/SearchForm/Index.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/Button";
+import { Button } from "@/components/Button/Index";
 import styles from "./searchform.module.css";
 
 export const SearchForm = () => {
